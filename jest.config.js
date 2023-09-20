fd772282dbc5f73cbc4167637a4ba307457ec24f
NatSama2 <vet.nat.c@gmail.com>
2023-09-20 19:09:31 -0300

    new api key

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@
 
 /** @type {import('jest').Config} */
 module.exports = {
-  // All imported modules in your tests should be mocked automatically
+  // All imported modules in your tests should be mocked automatically.
   // automock: false,
 
   // Stop running tests after `n` failures
